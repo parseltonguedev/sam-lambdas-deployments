09839ca8a405ce0bc6d8af70319012da4037961a
Bohdan Solianyk <bohdan_solianyk@epam.com>
2021-07-09 15:12:27 +0300

    initial commit

---
+++ lambda1/index.py
@@ -0,0 +1,20 @@
+import json
+from test import add
+
+print('Loading function')
+print('updating')
+a = 2
+b = 2
+
+
+def lambda_handler(event, context):
+    #print("Received event: " + json.dumps(event, indent=2))
+    print("value1 = " + event['key1'])
+    result = add(a, b)
+    return {
+        "statusCode": 200,
+        "body": {
+            "expression": result,
+            "event": event['key1']
+        }
+    }
---
+++ lambda2/index.py
@@ -0,0 +1,19 @@
+import json
+from test import add
+
+print('Loading function')
+a = 2
+b = 2
+
+
+def lambda_handler(event, context):
+    #print("Received event: " + json.dumps(event, indent=2))
+    print("value1 = " + event['key1'])
+    result = add(a, b)
+    return {
+        "statusCode": 200,
+        "body": {
+            "expression": result,
+            "event": event['key1']
+        }
+    }
